20b8c3855e471265b2af4e00f4d3bb891103fe23
Valery Chen <cpan3@stuy.edu>
2017-10-17 17:58:58 -0400

    can update avgs

--- stu_mean.py
+++ stu_mean.py
@@ -20,16 +20,18 @@ command = "SELECT name,mark FROM peeps,courses WHERE peeps.id=courses.id"
 stu_info = c.execute(command)
 
 # create a dictionary of grades
-grades = {}
+def populate(stu_info, grades = {}):
+    for info in stu_info:
+        name = info[0] #to include name
+        mark = info[1] #and mark
+        if name in grades:
+            grades[name].append(mark) #add to mark list of marks
+        else:
+            grades[name] = [] #list to hold marks
+            grades[name].append(mark) #add in mark
+    return grades
 
-for info in stu_info:
-    name = info[0]
-    mark = info[1]
-    if name in grades:
-        grades[name].append(mark)
-    else:
-        grades[name] = []
-        grades[name].append(mark)
+grades = populate(stu_info)
 
 '''
 print "TEST: "
@@ -39,6 +41,7 @@ for key in grades:
 '''
 
 '''2. Compute each student's average'''
+#computes average given a list
 def average(lst):
     total = 0;
     for item in lst:
@@ -57,8 +60,8 @@ for key in grades:
     print grades[key]
 '''
 
-
 '''3. Display each student's name, id, and average'''
+#insert id corresponding to name
 command = "SELECT name,id FROM peeps"
 stu_ids = c.execute(command)
 
@@ -66,10 +69,11 @@ for info in stu_ids:
     name = info[0]
     id = info[1]
     if name in grades:
-        grades[name].insert(0, id)
+        grades[name].insert(0, id) #insert id if name is already a key
+        #first value in the grade dict is now the id, second is the avg
     else:
         grades[name] = []
-        grades[name].insert(0, id) #first value in the grade dict is now the id, second is the avg
+        grades[name].insert(0, id) 
 
 #display info
 for key in grades: #key = name #note: must change floats/int avgs to str type in order to concatinate
@@ -77,24 +81,47 @@ for key in grades: #key = name #note: must change floats/int avgs to str type in
 
 
 '''4. Create a table of IDs and associated averages'''
-'''
-print "TEST: "
-for key in grades:
-    print key + ": "
-    print grades[key]
-'''
 
+#in case table has already been created
 try:
     command = "CREATE TABLE peeps_avg (id INTEGER PRIMARY KEY, average NUMERIC)"
     c.execute(command)
 except:
     print "TABLE AVERAGES ALREADY EXISTS"
 
-
+#in case values have already been inserted
 for key in grades:
-    command = "INSERT INTO peeps_avg VALUES(" + str(grades[key][0]) + "," + str(grades[key][1]) +")"
-    c.execute(command)
-    #print str(grades[key][0])
-    #print str(grades[key][1])
-#except:
-#    print "INSERTED VALUES ALREADY EXIST"
+    try:
+        command = "INSERT INTO peeps_avg VALUES(" + str(grades[key][0]) + "," + str(grades[key][1]) + ")"
+        #print command
+        c.execute(command)
+    except:
+        print "INSERTED VALUES ALREADY EXIST"
+
+'''5. Facilitate updating a student's average'''
+
+def update_avg():
+    for key in grades:
+        #get all values not id and avg
+        comp_list = grades[key][2: ]
+        avg = average(comp_list)
+        #replace now obsolete avg
+        grades[key].pop(1)
+        grades[key].insert(1, avg)
+    return
+
+update_avg()
+'''
+print "TEST: "
+for key in grades:
+    print key + ": "
+    print grades[key]
+'''
+    
+
+'''6. Facilitate adding rows to the courses table'''
+
+
+#==========================================================
+db.commit()
+db.close()
